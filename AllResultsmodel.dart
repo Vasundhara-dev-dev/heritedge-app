c112164c5e182f61a3f2cc236f7da7b6759b50e8
Vasundhara Bose <bosevasundhara9@gmail.com>
2025-09-21 23:23:58 +0530

    Create AllResultsmodel.dart

---
+++ AllResultsmodel.dart
@@ -0,0 +1,30 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'a_i_results_widget.dart' show AIResultsWidget;
+import 'package:styled_divider/styled_divider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class AIResultsModel extends FlutterFlowModel<AIResultsWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+  int get tabBarPreviousIndex =>
+      tabBarController != null ? tabBarController!.previousIndex : 0;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+  }
+}
